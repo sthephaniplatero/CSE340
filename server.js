ce33d99e9bc6da93b569018b6b90a3d544f93ba8
sthephaniplatero <pla21002@byui.edu>
2025-03-05 17:54:44 -0600

    update

--- server.js
+++ server.js
@@ -2,42 +2,41 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
-/* ***********************
+/******************************************
  * Require Statements
- *************************/
+ ******************************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+require("dotenv").config() 
+const app = express()  
+const staticRoutes = require("./routes/static")
 
-/* ***********************
- * Routes
- *************************/
-app.use(static)
-
-//Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
-
-/* ***********************
+/******************************************
  * View Engine and Templates
- *************************/
+ ******************************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+app.set("layout", "./layouts/layout")
 
-/* ***********************
+/******************************************
+ * Middleware y Rutas
+ ******************************************/
+app.use(staticRoutes)
+
+// Index route
+app.get("/", function(req, res) {
+  res.render("index", { title: "Home" })
+})
+
+/******************************************
  * Local Server Information
- * Values from .env (environment) file
- *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+ ******************************************/
+const port = process.env.PORT || 5500
+const host = process.env.HOST || "localhost"
 
-/* ***********************
+/******************************************
  * Log statement to confirm server operation
- *************************/
+ ******************************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`App listening on http://${host}:${port}`)
 })
